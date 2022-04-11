244043cb790812aeb3d1e1f1d5f16340cce3966a
elissa-murphy <elrmurph@iu.edu>
2022-04-10 22:20:55 -0400

    first init

---
+++ model/model.js
@@ -0,0 +1,240 @@
+// this information simulates getting data from a database 
+var homeContent = `<div class="hero">
+<div class="title">MARIAN UNIVERSITY</div>
+</div>
+<div class="content">
+<div class="stats">
+    <div class="majors">
+        <div class="number">40+</div>
+        <div class="number-title">majors</div>
+    </div>
+    <div class="students">
+        <div class="number">3,700+</div>
+        <div class="number-title">students</div>
+    </div>
+    <div class="campuses">
+        <div class="number">2</div>
+        <div class="number-title">campuses</div>
+    </div>
+    <div class="sports">
+        <div class="number">20+</div>
+        <div class="number-title">sports</div>
+    </div>
+</div>
+
+<div class="new-schools">
+    <div class="school1">
+        <img src="images/new-school.jpeg" alt="New School">
+        <p class="school1-words">Check Out Our Latest Launch:<br> &nbsp;&nbsp;The New E. S. Witchger School<br>&nbsp;&nbsp;of Engineering</p>
+        <a href="#" class="virtual-tour">Watch the Virtual Tour &#8594;</a>
+    </div>
+
+    <div class="school2">
+        <img src="images/new-college.jpeg" alt="New College" class="ancilla">
+        <p class="school2-words">Marian University's Ancilla<br>College is Here</p>
+        <a href="#" class="visit-website">Visit Ancilla's Website &#8594;</a>
+    </div>
+</div>
+
+<div class="support-students">
+    <img src="images/supportStudents.png" alt="Help Support our Students">
+    <p class="support-title">Help Support Our Students</p>
+    <p class="support-desc">Your donation helps our students become transformational leaders for<br> service to the world.</p>
+    <a href="#" class="support-our-students">Support Our Students &#8594;</a>
+    <a href="#" class="give-now">Give Now &#8594;</a>
+</div>
+
+<div class="choose-marian">
+    <div class="marian-title">Why Choose Marian?</div>
+    <div class="career-ready">
+        <div class="career-ready-title">You'll Be Career Ready</div>
+        <div class="career-ready-desc">Our academic, service, and co-curricular experiences prepare you for<br> life after graduation. Marian's Indianapolis location enables us to<br> connect you with major employers in business sectors ranging from<br> education to healthcare, the biosciences, pharmaceuticals, logistics and<br> distribution, advanced manufacturing, technology, and many other<br> industries.</div>
+    </div>
+</div>
+
+</div>`;
+
+var aboutContent = ` <div class="academics-hero">
+<div class="academics-title">ACADEMICS</div>
+</div>
+<div class="content">
+<div class="academic-images">
+    <div class="image1">
+        <img src="images/academics/academic1.jpeg" alt="">
+    </div>
+    <div class="image2">
+        <img src="images/academics/academic2.jpeg" alt="">
+    </div>
+    <div class="image3">
+        <img src="images/academics/academic3.jpeg" alt="">
+    </div>
+</div>
+
+<div class="schools-and-programs">
+    <div class="schools-and-programs-title">Schools and Programs</div>
+    
+    <div class="program-list">
+        <div class="program1">
+            <div class="program-title">Ancilla College of Marian University</div>
+            <div class="program-desc">Ancilla College of Marian University is committed to providing all students with a comprehensive educational experience.</div>
+        </div>
+
+        <div class="program2">
+            <div class="program-title">Byrum School of Business</div>
+            <div class="program-desc">We achieve excellence by educating transformational business leaders with hands on experience in order to become trustworthy, inquisitive, and skilled.</div>
+        </div>
+
+        <div class="program3">
+            <div class="program-title">College of Arts and Sciences</div>
+            <div class="program-desc">Students learn to be thinkers, writers, confident speakers, and strong researchers no matter where their career choices may take them.</div>
+        </div>
+    </div>
+
+    <div class="program-list">
+        <div class="program1">
+            <div class="program-title">College of Osteopathic Medicine</div>
+            <div class="program-desc">A Franciscan institution dedicated to the nurturing of mind, body, and spirit is the perfect place to pursue a career in healthcare.</div>
+        </div>
+
+        <div class="program2">
+            <div class="program-title">E. S. Witchger School of Engineering</div>
+            <div class="program-desc">In this technological and educational hub, engineering students will enjoy experiential, hands-on learning opportunities and connect with industry leaders in Indianapolis.</div>
+        </div>
+
+        <div class="program3">
+            <div class="program-title">Klipsch Educators College</div>
+            <div class="program-desc">Experience one of the most innovative teacher preparation programs in Indiana and graduate career-ready with the knowledge to serve as a teacher-leader who inspires young minds to make a difference.</div>
+        </div>
+    </div>
+
+    <div class="program-list">
+        <div class="program1">
+            <div class="program-title">Leighton School of Nursing</div>
+            <div class="program-desc">Thrive in this faith-based, family-friendly environment. And, most importantly, count on a faculty committed to your success as a student and as a future professional.</div>
+        </div>
+
+        <div class="program2">
+            <div class="program-title">Online programs (MAP)</div>
+            <div class="program-desc">Marian's Adult Program courses are 100% online and their accelerated nature means you'll earn your degree faster.</div>
+        </div>
+
+        <div class="program3">
+            <div class="program-title">Program in Exercise and Sports Science</div>
+            <div class="program-desc">Receive an outstanding student experience that's grounded in the latest advancements in physiology and biochemistry, which links the basic sciences to preventative medicine and health promotion.</div>
+        </div>
+    </div>
+</div>
+
+<div class="abroad-study">
+    <img src="images/academics/study-abroad.png" alt="Study Abroad">
+    <p class="study-abroad-title">Study Abroad</p>
+    <p class="study-abroad-desc">Studying abroad is a great way to enhance your appreciation of global communities.</p>
+    <a href="#" class="study-abroad-btn">Study Abroad &#8594;</a>
+    <a href="#" class="international">International Admissions &#8594;</a>
+    <a href="#" class="student-success">Student Success & Engagement &#8594;</a>
+</div>
+</div>`;
+
+var productsContent = ` <div class="student-life-hero">
+<div class="student-life-title">STUDENT LIFE</div>
+</div>
+<div class="content">
+<div class="life-on-campus">
+    <div class="life-on-campus-title">Life on Campus</div>
+    <div class="life-on-campus-desc">
+        <p>Whether you're a sports fan, art enthusiast, humanitarian, or bookworm, there's always plenty to do on Marian University's friendly<br> campus.</p>
+        <p>You can watch live theatre, root for your favorite Knights athletic teams, work out in the newly renovated fitness facility, take in an art<br> exhibit, help out in various community service projects, hear guest speakers, walk the trails of our 45-acre nature preserve, and more.<br> And, choose from more than 35 clubs on campus, from the Salsa Club to the Gaming Club to the Psychology Club. Join one or two<br>—or start your own!</p>
+    </div>
+</div>
+<div class="student-organizations">
+    <div class="student-organization-title">Join a Student Club/Organization</div>
+    <a href="#" class="governemnt">Student Government &#8594;</a>
+    <a href="#" class="clubs">Search All Student Clubs and Orgs &#8594;</a>
+</div>
+<div class="housing-options-title">Housing Options</div>
+<div class="housing-options" style="border-bottom: none;">
+    <div class="housing-option1">
+        <img src="images/studentLife/doyle-hall.jpeg" alt="Doyle Hall">
+        <p class="option1-title">Doyle Hall</p>
+        <p class="option1-desc">First-year students and Sophomores are<br> able to live here in single, double, or triple<br> occupancy rooms with a community<br> bathroom on each floor.</p>
+        <a href="#" class="option1-btn">Learn More &#8594;</a>
+    </div>
+    <div class="housing-option2">
+        <img src="images/studentLife/caito-wagner-hall.jpeg" alt="Caito Wagner Hall">
+        <p class="option2-title">Caito Wagner Hall</p>
+        <p class="option2-desc">First-years students stay here in double<br> rooms with a community bathroom on<br> each floor.</p>
+        <a href="#" class="option2-btn">Learn More &#8594;</a>
+    </div>
+    <div class="housing-option3">
+        <img src="images/studentLife/clare-hall.jpeg" alt="Clare Hall">
+        <p class="option3-title">Clare Hall</p>
+        <p class="option3-desc">Sophomores, Juniors and Seniors can be<br> in Clare Hall with a variety of living and<br> learning communities.</p>
+        <a href="#" class="option3-btn">Learn More &#8594;</a>
+    </div>
+</div>
+    <div class="housing-options">
+    <div class="housing-option1">
+        <img src="images/studentLife/drew-hall.jpeg" alt="Drew Hall">
+        <p class="option1-title">Drew Hall</p>
+        <p class="option1-desc">Upperclassmen live in Drew Hall in single,<br> double, or triple suite-style rooms with a<br> shared bathroom and private living room</p>
+        <a href="#" class="option1-btn">Learn More &#8594;</a>
+    </div>
+    <div class="housing-option2">
+        <img src="images/studentLife/university-hall-facade.jpeg" alt="Univeristy Hall">
+        <p class="option2-title">University Hall</p>
+        <p class="option2-desc">Upperclassmen live in University Hall with<br> single, double, and triple rooms that are<br> suite-style with a shared bathroom.</p>
+        <a href="#" class="option2-btn">Learn More &#8594;</a>
+    </div>
+    <div class="housing-option3">
+        <img src="images/studentLife/apartments-facade.jpeg" alt="University Apartments">
+        <p class="option3-title">University Houses & Apartments</p>
+        <p class="option3-desc">Students who are 21 and older and meet<br> GPA as well as Involvement criteria are<br> able to apply for University houses and<br> apartments.</p>
+        <a href="#" class="option3-btn">Learn More &#8594;</a>
+    </div>
+</div>
+</div>`;
+var contactContent = ` <div class="faith-hero">
+<div class="faith-title">FAITH AT MU</div>
+</div>
+<div class="content">
+<div class="values">
+    <div class="values-title">Our Values</div>
+    <div class="values-desc">
+        <p>Marian University's identity as a Catholic Franciscan institution is grounded in Jesus Christ. Our faith is ever-present and visible in the fabric of the<br> educational philosophy and operation of the university. It guides the way faculty, administration, staff, and students relate to one another and how visitors<br> are welcomed to campus.</p>
+        <ul>
+            <li>Our Catholic Franciscan identity guides what courses are offered, and the mode and purpose of the teaching and learning that occurs in the<br> classroom.</li>
+            <li>It guides the activities and tone of campus life, and most importantly, guides the university's journey into a remarkable future.</li>
+        </ul>
+        <br>
+        <p>Marian is a special place. As a student, it is a place to learn about who you really are and become the person you've always wanted to be. As a teacher,<br> you participate in an excellent academic environment where teaching and learning excellence merges with deeper values to create a challenging process of<br> intellectual, spiritual, moral, and social development.</p>
+    </div>
+</div>
+<div class="religious-art">
+    <div class="religous-art-title">Religious Art on Campus</div>
+    <img src="images/faith/religious-art.jpeg" alt="Religous Art on Campus"><br>
+    <a href="#" class="prayers">Religious Art and Prayer Spaces &#8594;</a>
+</div>
+<div class="mission-statement">
+    <div class="mission-title">Marian's Mission and Identity Statement</div>
+    <div class="mission-desc">
+        <p class="desc-title">Introduction</p>
+        <p class="desc">
+            Marian University is called to boldly serve the world by providing an education that transforms the hearts and minds of students through the integration of<br> knowledge and faith. As a Catholic Franciscan university we welcome all to join in this mission as students, staff, faculty, alumni, and friends.<br>
+            <br>
+            Our foundresses, the Sisters of Saint Francis (Oldenburg, IN), were ambassadors of Christ who walked by faith and whose courage to venture made our<br> great university possible. Today, this Gospel work is carried on in new ways by those who represent many faith systems and ideologies through excellent<br> teaching, individualized mentoring, and academically-rigorous research. At Marian University students receive an education that empowers them as leaders<br> to promote the Franciscan Sponsorship Values rooted in prayer: the dignity of the individual, reconciliation, peace, and justice, and responsible<br> stewardship.
+        </p>
+        <p class="desc-title">Aspects of a Great Catholic University</p>
+        <p class="desc">Marian University, the only Catholic university in central Indiana, traces its roots eight hundred years to the first Catholic universities and is grounded in its<br> Franciscan heritage. This tradition enables Marian University to provide the life-changing opportunity of education to students of every race, faith, ideology,<br> gender, and economic status in order to promote human progress. As Saint John Paul II stated when writing about higher education, "A Catholic University,<br> as any University, is immersed in human society; as an extension of its service to the Church and always within its proper competence, it is called on to<br> become an ever more effective instrument of cultural progress for individuals as well as for society." We dare to say each Marian University student will<br> experience the following aspects of a great Catholic university:</p>
+    </div>
+</div>
+<div class="upcoming-events">
+    <div class="upcoming-events-title">Religious Art on Campus</div>
+    <img src="images/faith/ministry.jpeg" alt="Ministry Events"><br>
+    <a href="#" class="ministry-events">View All Ministry Events &#8594;</a>
+</div>
+</div>`;
+
+//this function is being called from the app.js & it has the page name
+export function modelPageName(pgName) {
+    $("#app").html(eval(pgName));
+}
